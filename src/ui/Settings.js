59555d1facda5a3eba8b1fe7fc9e15001563528a
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2026-01-18 19:12:37 +0100

    Fixing window issue

--- src/ui/Settings.js
+++ src/ui/Settings.js
@@ -39,145 +39,157 @@ export default new class Settings {
         let packages;
 
         try {
-            packages =  JSON.parse(FileSystem.readFileSync(Utils.getPath('package.json')));
-        } catch(error) {
+            packages = JSON.parse(FileSystem.readFileSync(Utils.getPath('package.json')));
+        } catch (error) {
             /* Do Nothing */
         }
 
         IPC.on('settings', (event, packet) => {
-            switch(packet.action) {
+            switch (packet.action) {
                 case 'INIT':
                     this.send('VIRTHUE', {
-                        version:    packages?.version || 'N/A',
-                        electron:   Process?.versions?.electron || 'N/A',
-                        node:       Process?.versions?.node || 'N/A'
+                        version: packages?.version || 'N/A',
+                        electron: Process?.versions?.electron || 'N/A',
+                        node: Process?.versions?.node || 'N/A'
                     });
 
                     this.send('SETTINGS', this.Bridge.getConfiguration().toJSON());
                     this.send('ACCOUNTS', this.Bridge.getAuthentication().toJSON());
-                break;
+                    break;
                 case 'URL_OPEN':
-                    switch(packet.data?.data) {
+                    switch (packet.data?.data) {
                         case 'URL_PHILIPS':
                             Shell.openPath('https://www.philips.de');
-                        break;
+                            break;
                         case 'URL_PHILIPS_LIGTHNING':
                             Shell.openPath('https://www.lighting.philips.de/prof');
-                        break;
+                            break;
                         case 'URL_PHILIPS_HUE':
                             Shell.openPath('https://www.philips-hue.com/');
-                        break;
+                            break;
                         case 'URL_SIGNIFY':
                             Shell.openPath('https://www.signify.com/');
-                        break;
+                            break;
                         case 'GITHUB_ISSUE':
                             Shell.openPath('https://github.com/virthue/virthue/issues');
-                        break;
+                            break;
                         case 'GITHUB_REPOSITORY':
                             Shell.openPath('https://github.com/virthue/virthue');
-                        break;
+                            break;
                         default:
                             console.warn('Unknown URL instruction:', packet.data);
-                        break;
+                            break;
                     }
-                break;
+                    break;
                 case 'SETTINGS_SAVE':
                     let restart = false;
 
-                    switch(packet.data?.target) {
+                    switch (packet.data?.target) {
                         case 'bridge':
-                            if(packet.data?.model) {
+                            if (packet.data?.model) {
                                 this.Bridge.getConfiguration().setModel(packet.data?.model);
                             }
 
-                            if(packet.data?.name) {
+                            if (packet.data?.name) {
                                 this.Bridge.getConfiguration().setName(packet.data?.name);
                             }
 
-                            if(packet.data?.id) {
+                            if (packet.data?.id) {
                                 this.Bridge.getConfiguration().setId(packet.data?.id);
                             }
 
-                            if(packet.data?.mac) {
+                            if (packet.data?.mac) {
                                 this.Bridge.getConfiguration().setMACAddress(packet.data?.mac);
                             }
 
-                            if(packet.data?.port && this.Bridge.getConfiguration().getPort() !== Number(packet.data.port)) {
+                            if (packet.data?.port && this.Bridge.getConfiguration().getPort() !== Number(packet.data.port)) {
                                 this.Bridge.getConfiguration().setPort(packet.data.port);
                                 restart = true;
                             }
 
-                            if(packet.data?.tls && this.Bridge.getConfiguration().getSecuredPort() !== Number(packet.data.tls)) {
+                            if (packet.data?.tls && this.Bridge.getConfiguration().getSecuredPort() !== Number(packet.data.tls)) {
                                 this.Bridge.getConfiguration().setSecuredPort(packet.data.tls);
                                 restart = true;
                             }
 
                             this.Bridge.getConfiguration().store();
-                        break;
+                            break;
                         case 'flags':
-                            let old_secured     = this.Bridge.getConfiguration().supports(Support.SECURED);
+                            let old_secured = this.Bridge.getConfiguration().supports(Support.SECURED);
                             let old_description = this.Bridge.getConfiguration().supports(Support.DESCRIPTION);
 
-                            for(const support in Support) {
-                                let value           = Support[support];
-                                let active  = (typeof(packet.data[value]) !== 'undefined');
+                            for (const support in Support) {
+                                let value = Support[support];
+                                let active = (typeof (packet.data[value]) !== 'undefined');
 
-                                if(active) {
+                                if (active) {
                                     this.Bridge.getConfiguration().addSupportFlag(value);
                                 } else {
                                     this.Bridge.getConfiguration().removeSupportFlag(value);
                                 }
 
-                                if(value === Support.SECURED && old_secured !== active) {
+                                if (value === Support.SECURED && old_secured !== active) {
                                     restart = true;
                                 }
 
-                                if(value === Support.DESCRIPTION && old_description !== active) {
+                                if (value === Support.DESCRIPTION && old_description !== active) {
                                     restart = true;
                                 }
                             }
 
                             this.Bridge.getConfiguration().store();
-                        break;
+                            break;
                     }
 
-                    if(restart) {
+                    if (restart) {
                         console.log('Restarting Bridge...');
                     }
-                break;
+                    break;
             }
         });
 
-        if(!this.Window) {
-            this.Window = new BrowserWindow({
-                width: this.Size.width,
-                height: this.Size.height,
-                minWidth: this.Size.width,
-                minHeight: (this.Size.height / 2),
-                show: false,
-                frame: true,
-                fullscreenable: false,
-                resizable: true,
-                transparent: false,
-                webPreferences: {
-                    preload: Utils.getPath('src', 'ui', 'Preload.js'),
-                    contextIsolation: true,
-                    nodeIntegration: false
-                }
-            });
+        this.#createWindow();
+    }
 
-            this.Window.loadURL(`file://${Utils.getPath('assets', 'window', 'Settings.html')}`);
-            this.Window.setMenu(null);
-            this.Window.setIcon(Utils.getPath('assets', 'icons', 'logo.ico'));
+    #createWindow() {
+        if(this.Window && !this.Window.isDestroyed()) {
+            return;
         }
+
+        this.Window = new BrowserWindow({
+            width: this.Size.width,
+            height: this.Size.height,
+            minWidth: this.Size.width,
+            minHeight: (this.Size.height / 2),
+            show: false,
+            frame: true,
+            fullscreenable: false,
+            resizable: true,
+            transparent: false,
+            webPreferences: {
+                preload: Utils.getPath('src', 'ui', 'Preload.js'),
+                contextIsolation: true,
+                nodeIntegration: false
+            }
+        });
+
+        this.Window.loadURL(`file://${Utils.getPath('assets', 'window', 'Settings.html')}`);
+        this.Window.setMenu(null);
+        this.Window.setIcon(Utils.getPath('assets', 'icons', 'logo.ico'));
+
+        this.Window.on('closed', () => {
+            this.Window = null;
+        });
     }
 
     show() {
-        this.Window.show();
+        this.#createWindow();
+
+        this.Window.show(); //>>>>>>>>>>> Hilf mir hierbei: Object has been destroyed
     }
 
     send(name, data) {
-        if(!this.Window) {
+        if(!this.Window || this.Window.isDestroyed()) {
             return;
         }
 
